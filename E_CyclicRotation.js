26a7a431c12644a183fa308e60c17328783cf553
Mu-munn <rpgmukai333@gmail.com>
2022-04-21 13:25:29 +0900

    first commit

---
+++ E_CyclicRotation.js
@@ -0,0 +1,12 @@
+// 87%/
+
+
+function solution(A, K) {
+    if (A.length) {
+        for (let i = 0; i < K; i++) {
+            const pop = A.pop()
+            A.unshift(pop);
+        }
+    }
+    return A
+}
